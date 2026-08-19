4bc27c16f1ff3d6d96b71ad170147828a9497aa9
joaobertonha87 <joaoleiteribeiro87@gmail.com>
2026-08-19 15:49:12 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,55 @@
+
+const CACHE='jb-torneios-v4.5-20260819';
+const APP_SHELL=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './jb-logo-lime.png',
+  './jb-watermark-lime.png',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-512.png'
+];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+  const url=new URL(req.url);
+
+  if(req.mode==='navigate' || url.pathname.endsWith('/index.html')){
+    event.respondWith(
+      fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put('./index.html',copy));
+        return res;
+      }).catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(cached=>{
+      if(cached) return cached;
+      return fetch(req).then(res=>{
+        if(url.origin===location.origin){
+          const copy=res.clone();
+          caches.open(CACHE).then(c=>c.put(req,copy));
+        }
+        return res;
+      });
+    })
+  );
+});
