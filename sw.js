b14364415bcdce8f3e49ded46ac4f49c80c892b8
joaobertonha87 <joaoleiteribeiro87@gmail.com>
2026-08-22 12:33:48 -0300

    jb torneios fix

--- sw.js
+++ sw.js
@@ -1,35 +1,4 @@
-
-const CACHE='jb-torneios-v7-4-cloud-sync-20260820';
-const APP_SHELL=[
-  './','./index.html','./manifest.webmanifest',
-  './jb-logo-lime.png','./jb-watermark-lime.png',
-  './icon-180.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png'
-];
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)));
-  self.skipWaiting();
-});
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch',event=>{
-  const req=event.request;
-  if(req.method!=='GET') return;
-  const url=new URL(req.url);
-  if(req.mode==='navigate' || url.pathname.endsWith('/index.html')){
-    event.respondWith(fetch(req).then(res=>{
-      const copy=res.clone();
-      caches.open(CACHE).then(c=>c.put('./index.html',copy));
-      return res;
-    }).catch(()=>caches.match('./index.html')));
-    return;
-  }
-  event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{
-    if(url.origin===location.origin){
-      const copy=res.clone();
-      caches.open(CACHE).then(c=>c.put(req,copy));
-    }
-    return res;
-  })));
-});
+const VERSION='jb-v12-premium-20260822';
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil((async()=>{for(const k of await caches.keys()) await caches.delete(k);await self.clients.claim();})()));
+self.addEventListener('fetch',event=>{if(event.request.method==='GET') event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));});
