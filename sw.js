34fdf1790ec2b66a1ec4a6d10889ba3d196b9d82
joaobertonha87 <joaoleiteribeiro87@gmail.com>
2026-08-25 00:41:09 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-const CACHE='jb-torneios-v22-1';
+const CACHE='jb-torneios-v22';
 self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys()
+    .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    .then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
