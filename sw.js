8551691bf51209ca8cfe37a31e9ca034867f0f22
joaobertonha87 <joaoleiteribeiro87@gmail.com>
2026-08-26 13:30:03 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jb-torneios-v25-4';
+const CACHE='jb-torneios-v25-5';
 const CORE=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
@@ -6,13 +6,10 @@ self.addEventListener('fetch',e=>{
  if(e.request.method!=='GET')return;
  const q=e.request;
  if(q.mode==='navigate'){
-   e.respondWith(fetch(q,{cache:'no-store'}).then(r=>{
-     const x=r.clone(); caches.open(CACHE).then(c=>c.put('./index.html',x)).catch(()=>{}); return r;
-   }).catch(()=>caches.match('./index.html')));
-   return;
+  e.respondWith(fetch(q,{cache:'no-store'}).then(r=>{
+   const x=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',x)).catch(()=>{});return r;
+  }).catch(()=>caches.match('./index.html')));
+  return;
  }
- e.respondWith(caches.match(q).then(c=>c||fetch(q).then(r=>{
-   if(r&&r.ok){const x=r.clone();caches.open(CACHE).then(k=>k.put(q,x)).catch(()=>{})}
-   return r;
- })));
+ e.respondWith(caches.match(q).then(c=>c||fetch(q)));
 });
