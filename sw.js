210e4a68a3d362f5979fc7800b1bc2683e8f41c4
joaobertonha87 <joaoleiteribeiro87@gmail.com>
2026-09-02 12:33:15 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jb-torneios-v25-8-6';
+const CACHE='jb-torneios-v25-8-7';
 const CORE=['./','./index.html','./jb-logo-lime.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
