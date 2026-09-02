93895cf0ba4311dbcca68c5f202bb540a7d61150
joaobertonha87 <joaoleiteribeiro87@gmail.com>
2026-09-02 11:40:17 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='jb-torneios-v25-11-2';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png','./jb-logo-lime.png','./jb-watermark-lime.png'];
+const CACHE='jb-torneios-v25-8-2';
+const CORE=['./','./index.html','./jb-logo-lime.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const q=e.request;if(q.mode==='navigate'){e.respondWith(fetch(q,{cache:'no-store'}).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',x)).catch(()=>{});return r}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(q).then(c=>c||fetch(q)))});
